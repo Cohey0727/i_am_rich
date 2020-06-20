0f996060faae1ea7a6c77567fba5d944ca809ec1
Kohei <ohayousagi.ac.kook0727@gmail.com>
2020-06-20 14:54:45 +0900

    login_screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,76 @@
 import 'package:flutter/material.dart';
 
-// main function for starting point Flutter apps
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-    appBar: AppBar(
-      title: Text('I Am Rich'),
-      backgroundColor: Colors.blueGrey[100],
-    ),
-    backgroundColor: Colors.grey[900],
-    body: Center(
-      child: Image(image: AssetImage('images/diamond.png')),
-    ),
-  )));
+  runApp(MyApp());
 }
 
-//class MyApp extends StatelessWidget {
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      home: Scaffold(
-//        backgroundColor: Colors.blue,
-//        body: Text('Hello'),
-//      ),
-//    );
-//  }
-//}
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.blueAccent,
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            SizedBox(width: double.infinity),
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('images/face.jpg'),
+            ),
+            Text('Kohei Okamoto',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 28.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold)),
+            Text('Flutter Developer',
+                style: TextStyle(fontSize: 20, color: Colors.teal[100])),
+            FractionallySizedBox(
+                widthFactor: 0.8,
+                child: Container(
+              padding: EdgeInsets.fromLTRB(12, 8, 12, 8),
+              margin:  EdgeInsets.fromLTRB(12, 16, 12, 4),
+              color: Colors.white,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.phone_android,
+                    color: Colors.blueAccent,
+                  ),
+                  Text(
+                    '090-1234-5678',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            )),
+            FractionallySizedBox(
+                widthFactor: 0.8,
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(12, 8, 12, 8),
+                  margin:  EdgeInsets.fromLTRB(12, 8, 12, 4),
+                  color: Colors.white,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Icon(
+                        Icons.mail,
+                        color: Colors.blueAccent,
+                      ),
+                      Text(
+                        'example@test.com',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ))
+          ],
+        ),
+      ),
+    ));
+  }
+}
